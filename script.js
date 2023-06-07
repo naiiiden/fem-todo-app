c73f4bf81aa48a7780ebd625b9c24c728448b962
naiiiden <na4ko359@gmail.com>
2023-06-07 22:40:28 +0300

    create taskClass, push new task to array on form submit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('haha')
+const todoList = []
+
+const taskInput = document.querySelector('#task-input')
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(taskInput.value)
+
+    let newTask = new todoTask(taskInput.value, false)
+    todoList.push(newTask)
+})
+
+class todoTask {
+    constructor(task, completed = false) {
+        this.task = task
+        this.completed = completed
+    }
+}
