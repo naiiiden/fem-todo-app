4a986995521f3190911b07a3c9e16157516a76f3
naiiiden <na4ko359@gmail.com>
2023-06-07 22:57:44 +0300

    add dummy tasks and map them over

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-const todoList = []
+const todoList = [
+    {
+        task: 'task1',
+        completed: false
+    },
+    {
+        task: 'task2',
+        completed: false
+    },
+    {
+        task: 'task3',
+        completed: true
+    },
+    {
+        task: 'task4',
+        completed: false
+    },
+    {
+        task: 'task5',
+        completed: true
+    },
+    {
+        task: 'task6',
+        completed: false
+    },
+    {
+        task: 'task7',
+        completed: true
+    },
+    {
+        task: 'task8',
+        completed: true
+    },
+]
+
+const todoListMapped = todoList.map((task, index) => {
+    return `<div id=${index} key=${index}>task: ${task.task}, completed: ${task.completed}</div>`
+})
+
+document.querySelector('.tasks-container').innerHTML = todoListMapped.join('')
+
+
+
 
 const taskInput = document.querySelector('#task-input')
 const taskCompletedCheckbox = document.querySelector('#task-completed')
