10fb847c6ff5077fbc21d8efc942921b0a5f9871
naiiiden <na4ko359@gmail.com>
2023-06-07 22:48:41 +0300

    add task completed checkbox

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const todoList = []
 
 const taskInput = document.querySelector('#task-input')
+const taskCompletedCheckbox = document.querySelector('#task-completed')
+
+let taskCompleted = false
+
+taskCompletedCheckbox.addEventListener('change', () => {
+    taskCompleted = !taskCompleted
+})
 
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(taskInput.value)
+    console.log('task completed: ', taskCompleted)
 
-    let newTask = new todoTask(taskInput.value, false)
+    let newTask = new todoTask(taskInput.value, taskCompleted)
     todoList.push(newTask)
+    taskCompleted = false
+    taskCompletedCheckbox.checked = false
 })
 
 class todoTask {
