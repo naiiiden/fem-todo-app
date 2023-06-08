daee5f8416787fb135d7039566a908a0e98e48f0
naiiiden <na4ko359@gmail.com>
2023-06-08 09:06:41 +0300

    add unique id for each task

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
+let taskId = 0;
 const todoList = [
     {
         task: 'task1',
-        completed: false
+        completed: false,
+        id: taskId++
     },
     {
         task: 'task2',
-        completed: false
+        completed: false,
+        id: taskId++
     },
     {
         task: 'task3',
-        completed: true
+        completed: true,
+        id: taskId++
     },
     {
         task: 'task4',
-        completed: false
+        completed: false,
+        id: taskId++
     },
     {
         task: 'task5',
-        completed: true
+        completed: true,
+        id: taskId++
     },
     {
         task: 'task6',
-        completed: false
+        completed: false,
+        id: taskId++
     },
     {
         task: 'task7',
-        completed: true
+        completed: true,
+        id: taskId++
     },
     {
         task: 'task8',
-        completed: true
+        completed: true,
+        id: taskId++
     },
 ]
 
+
 const todoListMapped = todoList.map((task, index) => {
-    return `<div id=${index} key=${index}>task: ${task.task}, completed: ${task.completed}</div>`
+    return `
+        <div>
+            <input type='checkbox' class='complete-task-checkbox'/>
+            <span id=${index} key=${index}>task: ${task.task}, completed: ${task.completed}</span>
+            <button>delete</button>
+        </div>
+        `
 })
 
 document.querySelector('.tasks-container').innerHTML = todoListMapped.join('')
@@ -58,14 +74,26 @@ document.querySelector('form').addEventListener('submit', (e) => {
 
     let newTask = new todoTask(taskInput.value, taskCompleted)
     todoList.push(newTask)
-    document.querySelector('.tasks-container').innerHTML += `<div>task: ${taskInput.value}, completed: ${taskCompleted}</div>`
+    document.querySelector('.tasks-container').innerHTML += `
+        <div>
+            <input type='checkbox' class='complete-task-checkbox'/>    
+            <span>task: ${taskInput.value}, completed: ${taskCompleted}</span>
+            <button>delete</button>
+        </div>
+        `
     taskCompleted = false
     taskCompletedCheckbox.checked = false
+})
 
+document.querySelectorAll('.complete-task-checkbox').forEach(taskCheckbox => {
+    taskCheckbox.addEventListener('change', (e) => {
+        console.log(e.target.parentNode)
+    })
 })
 
 class todoTask {
     constructor(task, completed = false) {
+        this.id = taskId++
         this.task = task
         this.completed = completed
     }
