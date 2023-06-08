23250e339bcd8183fadf04aad7e214ce627288aa
naiiiden <na4ko359@gmail.com>
2023-06-08 09:31:31 +0300

    move class initialization to top, add dummy data the proper way

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+class TodoTask {
+    constructor(task, completed = false, id) {
+        this.task = task
+        this.completed = completed
+        this.id = id
+    }
+}
+
 let taskId = 0;
 const todoList = []
+
+todoList.push(new TodoTask('task1', false, taskId++))
+todoList.push(new TodoTask('task2', true, taskId++))
+todoList.push(new TodoTask('task3', false, taskId++))
+todoList.push(new TodoTask('task4', true, taskId++))
 
 const todoListMapped = todoList.map((task, index) => {
     return `
         <div id=task-${task.id}>
             <input type='checkbox' class='complete-task-checkbox'/>
             <span id=${index} key=${index}>task: ${task.task}, completed: ${task.completed}</span>
+            <button class='change-complete-button'>change complete status</button>
             <button>delete</button>
             ${task.id}
         </div>
@@ -28,12 +42,13 @@ document.querySelector('form').addEventListener('submit', (e) => {
     console.log(taskInput.value)
     console.log('task completed: ', taskCompleted)
 
-    let newTask = new todoTask(taskInput.value, taskCompleted, taskId++)
+    let newTask = new TodoTask(taskInput.value, taskCompleted, taskId++)
     todoList.push(newTask)
     document.querySelector('.tasks-container').innerHTML += `
         <div id=task-${taskId}>
             <input type='checkbox' class='complete-task-checkbox'/>    
             <span>task: ${taskInput.value}, completed: ${taskCompleted}</span>
+            <button class='change-complete-button'>change complete status</button>
             <button>delete</button>
             ${taskId}
         </div>
@@ -48,11 +63,3 @@ document.querySelectorAll('.complete-task-checkbox').forEach(taskCheckbox => {
         console.log(e.target.parentNode)
     })
 })
-
-class todoTask {
-    constructor(task, completed = false, id) {
-        this.task = task
-        this.completed = completed
-        this.id = id
-    }
-}
